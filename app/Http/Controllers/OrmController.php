9ad52ec6fbac30f2a28be75d5444f9c52928a6be
miguelcamilok <miguelcamilok@gmail.com>
2025-04-15 23:29:21 -0500

    consultas

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Apprentice;
+use App\Models\Area;
+use App\Models\Computer;
+use App\Models\Course;
+use App\Models\Teacher;
+use App\Models\TrainingCenter;
 use Illuminate\Http\Request;
 
 class OrmController extends Controller
 {
     //
     public function consultas(){
-        
+
+        // $area = Area::find(1);
+        // return $area->courses;
+
+        // $trainingCenter = TrainingCenter::find(1);
+        // return $trainingCenter->teachers;
+
+        // $teacher = Teacher::find(1);
+        // return $teacher->area;
+
+        // $course = Course::find(1);
+        // return $course->apprentices;
+
+        // $computer = Computer::find(1);
+        // return $computer->apprentice;
+
+        // $apprentice = Apprentice::find(1);
+        // return $apprentice->course;
+
+        return Teacher::with('Area.Courses.Apprentices.Computer')->get();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrmController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('orm', [OrmController::class, 'consultas']);
